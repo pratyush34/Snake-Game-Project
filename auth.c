9651965b417f11662ab491fd6608c00643f00d46
Mavn <mavn.arc@gmail.com>
2026-04-08 09:06:38 +0530

    add  UI menu, ascii art, and MCI mp3 audio engine

--- auth.c
+++ auth.c
@@ -1,186 +1,201 @@
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
 
-void readPassword(char *pass)
-{
-    int i = 0;
-    char c;
-    while ((c = getch()) != '\r') 
-    {
-        if (c == '\b') 
-        {
-            if (i > 0)
-            {
-                i--;
-                printf("\b \b");
-                fflush(stdout);
-            }
-        }
-        else if (i < 49)
-        {
-            pass[i++] = c;
-            printf("*");
-            fflush(stdout);
-        }
+void readPassword(char *pass) {
+  int i = 0;
+  char c;
+  while ((c = getch()) != '\r') {
+    if (c == '\b') {
+      if (i > 0) {
+        i--;
+        printf("\b \b");
+        fflush(stdout);
+      }
+    } else if (i < 49) {
+      pass[i++] = c;
+      printf("*");
+      fflush(stdout);
     }
-    pass[i] = '\0';
-    printf("\n");
+  }
+  pass[i] = '\0';
+  printf("\n");
 }
 
-void login(char *name)
-{
-    system("cls");
-    boundary();
-    gotoxy(1, 1);
-    printf("</SNAKE GAME>\n");
+void login(char *name) {
+  system("cls");
+  boundary();
+  gotoxy(1, 1);
+  printf("</SNAKE GAME>\n");
 
+  printf("%cENTER YOUR USERNAME : ", 179);
+  fflush(stdout);
+  scanf("%s", name);
 
-    printf("%cENTER YOUR USERNAME : ", 179);
+  FILE *f = fopen("data.txt", "a+");
+  char line[50], pass[50];
+  int score, found = 0;
+  rewind(f);
+
+  while (fscanf(f, "%[^,],%[^,],%d\n", line, pass, &score) == 3) {
+    if (strcmp(line, name) == 0) {
+      found = 1;
+      int attempts = 3;
+      int ok = 0;
+      while (attempts--) {
+        char entered[50];
+        printf("%cENTER PASSWORD : ", 179);
+        fflush(stdout);
+        readPassword(entered);
+
+        if (strcmp(entered, pass) == 0) {
+          ok = 1;
+          break;
+        } else {
+          printf("%cWrong password! %d attempt(s) left.\n", 179, attempts);
+          fflush(stdout);
+        }
+      }
+
+      if (!ok) {
+        printf("%cToo many wrong attempts. Exiting.\n", 179);
+        fflush(stdout);
+        fclose(f);
+        getch();
+        exit(1);
+      }
+
+      printf("%cWelcome back %s! Your last score : %d\n", 179, name, score);
+      fflush(stdout);
+      break;
+    }
+  }
+
+  if (!found) {
+    char newpass[50], confirm[50];
+
+    printf("%cNew user! Create a password : ", 179);
     fflush(stdout);
-    scanf("%s", name);
+    readPassword(newpass);
 
+    printf("%cConfirm password : ", 179);
+    fflush(stdout);
+    readPassword(confirm);
 
-    FILE *f = fopen("data.txt", "a+");
-    char line[50], pass[50];
-    int score, found = 0;
-    rewind(f);
+    while (strcmp(newpass, confirm) != 0) {
+      printf("%cPasswords do not match! Try again.\n", 179);
+      fflush(stdout);
 
-    while (fscanf(f, "%[^,],%[^,],%d\n", line, pass, &score) == 3)
-    {
-        if (strcmp(line, name) == 0)
-        {
-            found = 1;
-            int attempts = 3;
-            int ok = 0;
-            while (attempts--)
-            {
-                char entered[50];
-                printf("%cENTER PASSWORD : ", 179);
-                fflush(stdout);
-                readPassword(entered);
+      printf("%cCreate a password : ", 179);
+      fflush(stdout);
+      readPassword(newpass);
 
-                if (strcmp(entered, pass) == 0)
-                {
-                    ok = 1;
-                    break;
-                }
-                else
-                {
-                    printf("%cWrong password! %d attempt(s) left.\n",
-                           179, attempts);
-                    fflush(stdout);
-                }
-            }
-
-            if (!ok)
-            {
-                printf("%cToo many wrong attempts. Exiting.\n", 179);
-                fflush(stdout);
-                fclose(f);
-                getch();
-                exit(1);
-            }
-
-            printf("%cWelcome back %s! Your last score : %d\n",
-                   179, name, score);
-            fflush(stdout);
-            break;
-        }
+      printf("%cConfirm password  : ", 179);
+      fflush(stdout);
+      readPassword(confirm);
     }
 
-    /* --- New user: create password --- */
-    if (!found)
-    {
-        char newpass[50], confirm[50];
+    fprintf(f, "%s,%s,0\n", name, newpass);
+    printf("%cWelcome %s! Password saved.\n", 179, name);
+    fflush(stdout);
+  }
 
-        printf("%cNew user! Create a password : ", 179);
-        fflush(stdout);
-        readPassword(newpass);
-
-        printf("%cConfirm password : ", 179);
-        fflush(stdout);
-        readPassword(confirm);
-
-        while (strcmp(newpass, confirm) != 0)
-        {
-            printf("%cPasswords do not match! Try again.\n", 179);
-            fflush(stdout);
-
-            printf("%cCreate a password : ", 179);
-            fflush(stdout);
-            readPassword(newpass);
-
-            printf("%cConfirm password  : ", 179);
-            fflush(stdout);
-            readPassword(confirm);
-        }
-
-        fprintf(f, "%s,%s,0\n", name, newpass);
-        printf("%cWelcome %s! Password saved.\n", 179, name);
-        fflush(stdout);
-    }
-
-    fclose(f);
-    printf("%cUse W, A, S & D for movements.\n", 179);
-    printf("%cEnter any key to continue.", 179);
-    getch();
-    system("cls");
-    boundary();
+  fclose(f);
+  printf("%cUse W, A, S & D for movements.\n", 179);
+  printf("%cEnter any key to continue.", 179);
+  getch();
+  system("cls");
+  boundary();
 }
 
-void saveScore(char *name, int score)
-{
-    FILE *f = fopen("data.txt", "r");
-    FILE *temp = fopen("temp.txt", "w");
-    char line[50], pass[50];
-    int s;
+void saveScore(char *name, int score) {
+  FILE *f = fopen("data.txt", "r");
+  FILE *temp = fopen("temp.txt", "w");
+  char line[50], pass[50];
+  int s;
 
-    while (fscanf(f, "%[^,],%[^,],%d\n", line, pass, &s) == 3)
-    {
-        if (strcmp(line, name) == 0 && score > s)
-            fprintf(temp, "%s,%s,%d\n", name, pass, score);
-        else
-            fprintf(temp, "%s,%s,%d\n", line, pass, s);
-    }
+  while (fscanf(f, "%[^,],%[^,],%d\n", line, pass, &s) == 3) {
+    if (strcmp(line, name) == 0 && score > s)
+      fprintf(temp, "%s,%s,%d\n", name, pass, score);
+    else
+      fprintf(temp, "%s,%s,%d\n", line, pass, s);
+  }
 
-    fclose(f);
-    fclose(temp);
-    remove("data.txt");
-    rename("temp.txt", "data.txt");
+  fclose(f);
+  fclose(temp);
+  remove("data.txt");
+  rename("temp.txt", "data.txt");
 }
 
-void showLeaderboard()
-{
-    FILE *f = fopen("data.txt", "r");
-    char names[100][50], passes[100][50];
-    int scores[100], count = 0;
+void showLeaderboard() {
+  FILE *f = fopen("data.txt", "r");
+  char names[100][50], passes[100][50];
+  int scores[100], count = 0;
 
-    while (fscanf(f, "%[^,],%[^,],%d\n",
-                  names[count], passes[count], &scores[count]) == 3)
-        count++;
-    fclose(f);
+  while (fscanf(f, "%[^,],%[^,],%d\n", names[count], passes[count],
+                &scores[count]) == 3)
+    count++;
+  fclose(f);
 
-    for (int i = 0; i < count - 1; i++)
-        for (int j = 0; j < count - i - 1; j++)
-            if (scores[j] < scores[j + 1])
-            {
-                int tmp = scores[j];
-                scores[j] = scores[j + 1];
-                scores[j + 1] = tmp;
+  for (int i = 0; i < count - 1; i++)
+    for (int j = 0; j < count - i - 1; j++)
+      if (scores[j] < scores[j + 1]) {
+        int tmp = scores[j];
+        scores[j] = scores[j + 1];
+        scores[j + 1] = tmp;
 
-                char t[50];
-                strcpy(t, names[j]);
-                strcpy(names[j], names[j + 1]);
-                strcpy(names[j + 1], t);
+        char t[50];
+        strcpy(t, names[j]);
+        strcpy(names[j], names[j + 1]);
+        strcpy(names[j + 1], t);
 
-                strcpy(t, passes[j]);
-                strcpy(passes[j], passes[j + 1]);
-                strcpy(passes[j + 1], t);
-            }
+        strcpy(t, passes[j]);
+        strcpy(passes[j], passes[j + 1]);
+        strcpy(passes[j + 1], t);
+      }
 
-    printf("\n%c=== LEADERBOARD ===\n", 179);
-    for (int i = 0; i < count && i < 5; i++)
-        printf("%c%d. %s - %d\n", 179, i + 1, names[i], scores[i]);
+  printf("\n%c=== LEADERBOARD ===\n", 179);
+  for (int i = 0; i < count && i < 5; i++)
+    printf("%c%d. %s - %d\n", 179, i + 1, names[i], scores[i]);
+}
+
+// added functions
+
+void changePasswordUI(char *name) {
+  char newPass[50];
+  system("cls");
+
+  setColor(14);
+  printf("\n\t    CHANGE PASSWORD    \n");
+  setColor(11);
+  printf("\tUser: %s\n", name);
+  setColor(15);
+  printf("\tEnter new password: ");
+
+  scanf("%s", newPass);
+
+  FILE *f = fopen("data.txt", "r");
+  FILE *temp = fopen("temp.txt", "w");
+  char user[50], pass[50];
+  int score;
+
+  while (fscanf(f, "%[^,],%[^,],%d\n", user, pass, &score) == 3) {
+    if (strcmp(user, name) == 0) {
+      fprintf(temp, "%s,%s,%d\n", user, newPass, score);
+    } else {
+      fprintf(temp, "%s,%s,%d\n", user, pass, score);
+    }
+  }
+  fclose(f);
+  fclose(temp);
+
+  remove("data.txt");
+  rename("temp.txt", "data.txt");
+
+  setColor(10);
+  printf("\n\tPassword successfully updated!");
+  setColor(7);
+  printf("\n\tPress any key to return...");
+  getch();
 }
--- main.c
+++ main.c
@@ -1,16 +1,40 @@
+#include "aaextra.c"
+#include "auth.c"
+#include "gamePlay.c"
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#include <time.h>
+// #include <time.h>
 #include <windows.h>
-#include "gamePlay.c"
-#include "auth.c"
 
-int main() // huehueheu
-{
-    system("mode con: cols=120 lines=30");
-    char name[50];
-    login(name);
-    game(name);
-    return 0;
+int main() {
+  system("mode con: cols=120 lines=30");
+  char name[50];
+
+  splashScreen();
+  playBGM();
+  login(name);
+
+  while (1) {
+    int selection = mainMenu(); // Show interactive menu
+
+    switch (selection) {
+    case 1:
+      stopBGM();
+      game(name);
+      break;
+    case 2:
+      system("cls");
+      showLeaderboard();
+      printf("\n\tPress any key to return...");
+      getch();
+      break;
+    case 3:
+      changePasswordUI(name);
+      break;
+    case 4:
+      exit(0);
+    }
+  }
+  return 0;
 }
